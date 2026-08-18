3dd1bee636c8812105d0169b1cd20e7df7e70c7e
ishannegi8979-creator <ishannegi8979@gmail.com>
2026-08-18 16:29:56 +0530

    .\solidrectangle.cpp

--- solidrectangle.cpp
+++ solidrectangle.cpp
@@ -2,11 +2,13 @@
 using namespace std ;
 
 int main() {
-
+// outer loop for counting rows
     for (int row = 0; row  < 3; row = row + 1)
     {
+        //! inner loop for colunms
         for(int column = 0 ; column < 5 ; column = column + 1  )
         {
+            //* printing *
             cout << "* ";
         }
         cout << endl;

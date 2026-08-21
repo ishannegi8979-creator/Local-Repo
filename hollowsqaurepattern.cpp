f98f2da6b7bb19c8496f2f44d9197c2b1be27ac8
ishannegi8979-creator <ishannegi8979@gmail.com>
2026-08-21 19:04:25 +0530

    .\hollowsqaurepattern.cpp

---
+++ hollowsqaurepattern.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std ;
+
+int main () 
+{
+  for ( int row = 0 ; row < 4 ; row = row + 1)
+   {
+
+    if( row == 0 || row == 3)
+    {
+        for ( int i = 0 ; i < 4 ; i = i+1)
+        {
+            cout << "* ";
+        }
+
+    }
+    else {
+
+        cout << "* ";
+    
+    for ( int r = 0 ; r < 2 ; r = r+1)
+    {
+        cout << "  ";
+    }
+    
+    cout << "* ";
+}
+        cout << endl ;
+
+   }
+}
